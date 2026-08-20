af1fcf7a54a3d4e9d3a80e2139af95593a77a457
Dizzy B High <dizzybhigh@gmail.com>
2026-08-20 09:37:03 +0200

    Make timeout cells match key initiator data by target id

--- assets/script.js
+++ assets/script.js
@@ -159,6 +159,7 @@ function createCell(d) {
   const e = document.createElement("div");
   e.className = "cell";
   e.dataset.platform = d.banWidgetPlatform || "unknown";
+  e.dataset.userId = String(d.banWidgetTargetId || d.userId || d.id || "");
   e.innerHTML = '<img class="avatar" alt=""><div class="bars"><i class="bar"></i><i class="bar"></i><i class="bar"></i><i class="bar"></i><i class="bar"></i><i class="bar"></i><i class="bar"></i></div><img class="frame" src="assets/cell_frame_final.png" alt=""><div class="timer">00:00</div><div class="nameplate"><span></span></div><div class="reason"><span></span></div>';
   const a = e.querySelector(".avatar");
   a.src = d.banWidgetTargetAvatar || d.avatar || d.profileImageUrl || d.targetUserProfileImageUrl || d.userProfileImageUrl || "";
